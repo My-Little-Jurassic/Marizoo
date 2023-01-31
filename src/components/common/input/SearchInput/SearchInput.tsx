6af62a5ff338aa2b791cf8b2afa6d788ced3a8d1
한유경 <ygo65312@naver.com>
2023-01-31 12:41:05 +0900

    add : SearchInput component

---
+++ src/components/common/input/SearchInput/SearchInput.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import { TbSearch } from "react-icons/tb";
+
+const StyledDiv = styled.div`
+  box-sizing: border-box;
+  width: 516px;
+  height: 60px;
+  border: 4px solid ${({ theme }) => theme.colors.green};
+  border-radius: 32px;
+  padding: auto 28px;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  ${({ theme }) => theme.shadow}; //shadow CSS
+  background-color: ${({ theme }) => theme.colors.secondaryBg};
+
+  & > label svg {
+    font-size: 25px;
+    margin: auto 8px auto 24px;
+    stroke-width: 3px;
+    color: ${({ theme }) => theme.colors.green};
+  }
+
+  & > input {
+    width: 424px;
+    height: 100%;
+    border: none;
+    background: none;
+    font: ${({ theme }) => theme.fonts.subContentBold};
+    color: ${({ theme }) => theme.colors.primaryText};
+
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      font: ${({ theme }) => theme.fonts.subContentBold};
+      color: ${({ theme }) => theme.colors.brandColors.basaltGray[400]};
+    }
+  }
+`;
+
+interface IProps {
+  value?: string;
+  setValue(value: string): void;
+  placeholder?: string;
+  onSearch(value: string): void;
+}
+
+const SearchInput = ({ value = "", setValue, placeholder = "", onSearch }: IProps): JSX.Element => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const onKeyup = (e: React.KeyboardEvent & React.ChangeEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSearch(e.target.value);
+  };
+
+  return (
+    <StyledDiv>
+      <label>
+        <TbSearch />
+      </label>
+      <input defaultValue={value} placeholder={placeholder} onChange={onChange} onKeyUp={onKeyup} />
+    </StyledDiv>
+  );
+};
+
+export default React.memo(SearchInput);
