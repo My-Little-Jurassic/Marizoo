f6be82c2c47bde6700521e2d2ec15469bbf0b176
김승희 <kanada11416@gmail.com>
2023-01-31 14:53:46 +0900

    fix : merge

--- user/src/main/java/com/marizoo/user/entity/AnimalStore.java
+++ user/src/main/java/com/marizoo/user/entity/AnimalStore.java
@@ -1,29 +1,5 @@
 package com.marizoo.user.entity;
 
-<<<<<<< HEAD
-import lombok.Getter;
-
-import javax.persistence.*;
-import java.sql.Time;
-
-@Entity
-@Table(name = "animal_store")
-@Getter
-public class AnimalStore {
-    @Id @GeneratedValue
-    @Column(name = "animal_store_id")
-    private Long id;
-
-    private String storeName;           // 가게 이름
-    private String description;         // 가게 설명
-    private String address;             // 가게 주소
-    private Time openingHours;          // 가게 영업 시간
-    private String tel;                 // 가게 전화번호
-    private String email;               // 가게 이메일
-    private String profileImg;          // 가게 사진
-    private String lat;                 // 가게 위도
-    private String lng;                 // 가게 경도
-=======
 import com.marizoo.user.entity.common.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -64,5 +40,5 @@ public class AnimalStore extends BaseEntity {
 
     @OneToMany(mappedBy = "animalStore")
     private List<Broadcast> broadcastList = new ArrayList<>();
->>>>>>> 043d037fef92b98b56fa9f9a2c94b43ce2a58bee
+
 }
--- user/src/main/java/com/marizoo/user/entity/Broadcast.java
+++ user/src/main/java/com/marizoo/user/entity/Broadcast.java
@@ -1,33 +1,5 @@
 package com.marizoo.user.entity;
 
-<<<<<<< HEAD
-import lombok.Getter;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name="broadcast")
-@Getter
-public class Broadcast {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "broadcast_id")
-    private Long id;
-
-    private String title;                   // 방송 제목
-    private String broadcast_type;          // 방송 유형
-    private String description;             // 방송 설명
-    private String thumbnail;               // 방송 썸네일
-    private LocalDateTime startTime;        // 방송 시작 시간
-    private LocalDateTime endTime;          // 방송 종료 시간
-
-    @Enumerated(EnumType.STRING)
-    private BroadcastStatus status;         // 방송 상태
-
-    @ManyToOne
-    @JoinColumn(name = "animal_store_id")
-    private AnimalStore animalStore;           // 방송 주체
-=======
 import com.marizoo.user.entity.common.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -68,5 +40,5 @@ public class Broadcast extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vote_id")
     private Vote vote;
->>>>>>> 043d037fef92b98b56fa9f9a2c94b43ce2a58bee
+
 }
