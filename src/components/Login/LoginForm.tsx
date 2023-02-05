7ce38f4069b187dc7cda6645594abdd1b08cc011
한유경 <ygo65312@naver.com>
2023-02-06 03:47:33 +0900

    update : login 이벤트 추가

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,16 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import {
-  validateEmail,
-  validateIdChar,
-  validateIdLength,
-  validatePWChar,
-  validatePWLength,
-} from "../../utils/registerValidation";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { login, selectUser } from "../../store/userSlice";
 import { GreenBtn } from "../common/button";
 import { IInputVerify, Input, VerifyInput } from "../common/input";
 
-const StyledForm = styled.form`
+const StyledForm = styled.form<{ status: string }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -23,8 +19,14 @@ const StyledForm = styled.form`
     margin-bottom: 32px;
   }
 
+  & > .notice {
+    font: ${({ theme }) => theme.fonts.tinyContentBold};
+    color: ${({ theme }) => theme.colors.red};
+    opacity: ${({ status }) => (status === "failed" ? 1 : 0)};
+    margin: 24px auto;
+  }
   & > .submit-btn {
-    margin: 60px auto;
+    margin: 0 auto 60px;
     width: 148px;
     & > button {
       width: 100%;
@@ -50,11 +52,18 @@ interface IInput {
 }
 
 const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+  const navigate = useNavigate();
   const [data, setData] = useState<IRegistForm>({
     uid: "",
     pwd: "",
     validation: false,
   });
+
+  useEffect(() => {
+    if (user.isUser) navigate("/");
+  }, [user.isUser]);
 
   const updateData = (id: RegistInputId, value: string) => {
     const newData = { ...data };
@@ -66,6 +75,8 @@ const LoginForm = () => {
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     // TODO: 로그인 POST요청
+    const { uid, pwd } = data;
+    dispatch(login({ uid, pwd }));
   };
 
   const inputList: IInput[] = [
@@ -83,7 +94,7 @@ const LoginForm = () => {
   ];
 
   return (
-    <StyledForm onSubmit={onSubmit}>
+    <StyledForm onSubmit={onSubmit} status={user.status}>
       {inputList.map((item, index) => {
         const { id, type, ref, placeholder, inputVerifyList } = item;
         if (inputVerifyList)
@@ -95,7 +106,7 @@ const LoginForm = () => {
               type={type}
               placeholder={placeholder}
               inputVerifyList={inputVerifyList}
-              submitInputResult={(val) => {
+              submitInputResult={function (val) {
                 updateData(id, val);
               }}
             />
@@ -108,12 +119,13 @@ const LoginForm = () => {
               value={data[id]}
               type={type}
               placeholder={placeholder}
-              setValue={(val) => {
+              setValue={function (val) {
                 updateData(id, val);
               }}
             />
           );
       })}
+      <span className="notice">아이디와 비밀번호를 확인해주세요.</span>
       <div className="submit-btn">
         <GreenBtn label={"로그인"} type={0} isDisable={false} />
       </div>
