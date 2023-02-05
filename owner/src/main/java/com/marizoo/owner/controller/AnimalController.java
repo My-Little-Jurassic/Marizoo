370bf7217808050508704e9f6917e17ae1d86dfb
seunghee <kanada11416@gmail.com>
2023-02-06 01:01:25 +0900

    update : response 형식 변환

--- owner/src/main/java/com/marizoo/owner/controller/AnimalController.java
+++ owner/src/main/java/com/marizoo/owner/controller/AnimalController.java
@@ -1,6 +1,7 @@
 package com.marizoo.owner.controller;
 
 import com.marizoo.owner.api.request.AnimalFeedRequest;
+import com.marizoo.owner.api.response.FeedListResponse;
 import com.marizoo.owner.dto.FeedDto;
 import com.marizoo.owner.entity.Feed;
 import com.marizoo.owner.service.FeedService;
@@ -11,19 +12,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-public class AnimalContoller {
+public class AnimalController {
     private final FeedService feedService;
 
     @PostMapping("/animals/feeds")
     public ResponseEntity<?> getFeedListforAnimals(@RequestBody AnimalFeedRequest animalFeedRequest) {
         List<Feed> animalFeedList = feedService.findFeedListforAnimals(animalFeedRequest.getAnimalIdList());
         List<FeedDto> result = animalFeedList.stream().map(af -> new FeedDto(af.getId(), af.getName(), af.getImg())).collect(Collectors.toList());
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(new FeedListResponse(result), HttpStatus.OK);
     }
 }
