f146065b4ff2cb383ac6e55ce86971c345ad710f
seungbok3240 <seungbok3240@gmail.com>
2023-02-05 11:50:46 +0900

    fix : same file delete userPlayRepository

--- user/src/main/java/com/marizoo/user/repository/UsersPlayRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marizoo.user.repository;
-
-import com.marizoo.user.entity.UsersPlay;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersPlayRepository extends JpaRepository<UsersPlay, Long> {
-}
