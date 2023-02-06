b0c4ec0d3af34f6e2bea5a84a126f898bc7fa27a
seungbok3240 <seungbok3240@gmail.com>
2023-02-06 14:29:45 +0900

    add : S3 config

---
+++ user/src/main/java/com/marizoo/user/controller/FileController.java
@@ -0,0 +1,23 @@
+package com.marizoo.user.controller;
+
+import com.marizoo.user.util.AwsS3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class FileController {
+
+    private final AwsS3Uploader awsS3Uploader;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        String fileName = awsS3Uploader.upload(multipartFile, "test");
+        return fileName;
+    }
+}
