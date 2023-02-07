c9c947454ce7a96eaa15f66d33fcbb741ce02f35
seunghee <kanada11416@gmail.com>
2023-02-07 13:10:03 +0900

    add : swagger. (broadcast, store)

--- user/src/main/java/com/marizoo/user/controller/BroadcastController.java
+++ user/src/main/java/com/marizoo/user/controller/BroadcastController.java
@@ -6,6 +6,9 @@ import com.marizoo.user.api.broadcast_api.OnairApi;
 import com.marizoo.user.dto.broadcast_dto.*;
 import com.marizoo.user.entity.*;
 import com.marizoo.user.service.BroadcastService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ import java.util.List;
 public class BroadcastController {
     private final BroadcastService broadcastService;
 
+    @ApiOperation(value= "현재 방송 중인 목록 가져오기")
     @GetMapping("/broadcasts")
     public ResponseEntity<?> getOnairs(){
         // 현재 방송 중인 목록 가져오기
@@ -37,8 +41,9 @@ public class BroadcastController {
         return new ResponseEntity<>(new OnairApi(result), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "broadcast_id에 해당하는 방송 정보를 가져오기", notes = "방송 정보, 방송 출연 동물 정보, 방송 가게 정보")
     @GetMapping("/broadcasts/{broadcast_id}")
-    public ResponseEntity<?> getBroadcastInfo(@PathVariable("broadcast_id") Long broadcastId){
+    public ResponseEntity<?> getBroadcastInfo(@PathVariable("broadcast_id") @ApiParam(name = "방송 id", required = true, example = "1") Long broadcastId){
         // broadcast_id에 해당하는 방송 정보 가져오기.
 
         Broadcast broadcast = broadcastService.getBroadcast(broadcastId);
@@ -66,8 +71,9 @@ public class BroadcastController {
         return new ResponseEntity<BroadcastApi>(new BroadcastApi(broadcastDto, animals, animalStoreDto), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "broadcast_id에 해당하는 vote 정보 가져오기")
     @GetMapping("/broadcasts/{broadcast_id}/vote")
-    public ResponseEntity<?> getBroadcastVote(@PathVariable("broadcast_id") Long broadcastId){
+    public ResponseEntity<?> getBroadcastVote(@PathVariable("broadcast_id") @ApiParam(name = "방송 id", required = true, example = "1") Long broadcastId){
 //        feedvote 정보
         List<FeedVote> feedVoteList = broadcastService.getFeedVote(broadcastId);
 
@@ -84,9 +90,9 @@ public class BroadcastController {
         return new ResponseEntity<FeedVoteApi>(new FeedVoteApi(result), HttpStatus.OK);
 
     }
-
+@ApiOperation(value = "keyword에 해당하는 종이 방송에 출연하는 현재 방송중인 방송 목록 가져오기")
     @GetMapping("/broadcasts/search")
-    public ResponseEntity<?> getSearchOnairs(@RequestParam(value = "keyword") String keyword){
+    public ResponseEntity<?> getSearchOnairs(@RequestParam(value = "keyword") @ApiParam(name = "검색어", required = true)String keyword){
         List<SearchBroadcastDto> searchOnairs = broadcastService.searchOnAirsHavingSpeciesList(keyword);
         if(searchOnairs.isEmpty()){
             return new ResponseEntity<>("검색 결과 없음", HttpStatus.BAD_REQUEST);
@@ -94,8 +100,9 @@ public class BroadcastController {
         return new ResponseEntity<>(new OnairApi(searchOnairs), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "broadcast_id 방송과 연관된 방송 목록 가져오기")
     @GetMapping("/broadcasts/{broadcast_id}/related")
-    public ResponseEntity<?> getRelatedOnairs(@PathVariable("broadcast_id") Long broadcastId){
+    public ResponseEntity<?> getRelatedOnairs(@PathVariable("broadcast_id") @ApiParam(name = "방송 id", required = true, example = "1")Long broadcastId){
         List<SearchBroadcastDto> relatedOnairs = broadcastService.searchBroadcastRelated(broadcastId);
         if(relatedOnairs.isEmpty()){
             return new ResponseEntity<>("관련 방송 없음", HttpStatus.BAD_REQUEST);
--- user/src/main/java/com/marizoo/user/controller/StoreController.java
+++ user/src/main/java/com/marizoo/user/controller/StoreController.java
@@ -10,6 +10,8 @@ import com.marizoo.user.entity.Broadcast;
 import com.marizoo.user.entity.BroadcastAnimal;
 import com.marizoo.user.entity.Play;
 import com.marizoo.user.service.*;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -31,6 +33,7 @@ public class StoreController {
     private final SpeciesService speciesService;
     private final ReservationService reservationService;
 
+    @ApiOperation(value = "동물 가게 목록 가져오기")
     @GetMapping("/stores")
     public ResponseEntity<AnimalStoreListResponse> storeList(){
         List<AnimalStore> AnimalStoreList = animalStoreService.findAnimalStores();
@@ -47,11 +50,11 @@ public class StoreController {
         return new ResponseEntity<>( new AnimalStoreListResponse(AnimalStoreDtoList), HttpStatus.OK);
     }
 
-
+    @ApiOperation(value = "[storename, classification]으로 검색한 동물 가게 목록 가져오기")
     @GetMapping("/stores/search")
     public ResponseEntity<AnimalStoreListResponse> store_search(
-            @RequestParam(name = "storename", required = false, defaultValue = "") String storename,
-            @RequestParam(name = "classification", required = false, defaultValue = "") String species){
+            @RequestParam(name = "storename", required = false, defaultValue = "") @ApiParam(name = "가게명 검색어", required = false)String storename,
+            @RequestParam(name = "classification", required = false, defaultValue = "") @ApiParam(name = "종 검색어", required = false)String species){
 
         List<AnimalStore> storeList = new ArrayList<>();
         List<AnimalStoreDto> AnimalStoreDtoList;
@@ -84,30 +87,34 @@ public class StoreController {
     }
 
     // 팔로우
+    @ApiOperation(value = "store_id 동물 가게를 follow")
     @PostMapping("/stores/{store_id}")
-    public ResponseEntity<String> follow(@PathVariable(name = "store_id") Long store_id,
-                                         @RequestBody FollowRequest followRequest){
+    public ResponseEntity<String> follow(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long store_id,
+                                         @RequestBody FollowRequest followRequest) {
         animalStoreService.followingStore(store_id, followRequest.getUid());
         return new ResponseEntity<>("성공", HttpStatus.OK);
     }
 
 
     // 해당 가게 전체 정보 제공
+    @ApiOperation(value = "store_id 동물 가게 정보 가져오기")
     @GetMapping("/stores/{store_id}")
-    public ResponseEntity<AnimalStoreWholeDto> reserveToStore(@PathVariable(name = "store_id") Long store_id){
+    public ResponseEntity<AnimalStoreWholeDto> reserveToStore(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long store_id){
         return new ResponseEntity<>(animalStoreService.findAnimalStore(store_id), HttpStatus.OK);
     }
 
     // 해당 가게 있는 동물 목록 조회.
+    @ApiOperation(value = "store_id가 보유한 동물 목록 가져오기")
     @GetMapping("/stores/{store_id}/animals")
-    public ResponseEntity<AnimalListResponse> getOwnedAnimal(@PathVariable(name = "store_id") Long store_id){
+    public ResponseEntity<AnimalListResponse> getOwnedAnimal(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long store_id){
         AnimalListResponse OwnedAnimalList = new AnimalListResponse(animalService.findOwnedAnimal(store_id));
         return new ResponseEntity<>(OwnedAnimalList, HttpStatus.OK);
     }
 
     // 스트리밍 중인 영상 목록 조회
+    @ApiOperation(value = "store_id가 방송 중인 영상 목록 가져오기")
     @GetMapping("/stores/{store_id}/broadcasts")
-    public ResponseEntity<BroadcastListResponse> getOnairBroadcast(@PathVariable(name = "store_id")Long store_id){
+    public ResponseEntity<BroadcastListResponse> getOnairBroadcast(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long store_id){
         List<Broadcast> onairs = broadcastService.getOnAirs();
         List<BroadcastsDto> result = new ArrayList<>();
         for (Broadcast onair : onairs) {
@@ -124,8 +131,9 @@ public class StoreController {
 
 
     // 가게 체험 프로그램 목록 제공
+    @ApiOperation(value = "store_id가 진행하는 가게 체험 프로그램 목록 가져오기")
     @GetMapping("/stores/{store_id}/plays")
-    public ResponseEntity<StorePlayListResponse> getPlayInProgress(@PathVariable(name = "store_id") Long store_id){
+    public ResponseEntity<StorePlayListResponse> getPlayInProgress(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long store_id){
         List<Play> playlist = animalStoreService.findPlayByStore(store_id);
         List<StorePlayDto> playDtoList = playlist.stream()
                 .map(m -> new StorePlayDto(
@@ -139,14 +147,16 @@ public class StoreController {
     }
 
     // 체험 프로그램에 대한 정보 제공.
+    @ApiOperation(value = "store_id 가게가 제공하는 play_id 체험 프로그램에 대한 정보 가져오기")
     @GetMapping("/stores/{store_id}/plays/{play_id}")
-    public ResponseEntity<PlayAndStoreInfoResponse> getPlayInfo(@PathVariable(name = "store_id") Long storeId,
-                                                                @PathVariable(name = "play_id") Long playId){
+    public ResponseEntity<PlayAndStoreInfoResponse> getPlayInfo(@PathVariable(name = "store_id") @ApiParam(name = "동물가게 id", required = true, example = "1") Long storeId,
+                                                                @PathVariable(name = "play_id") @ApiParam(name = "체험 프로그램 id", required = true, example = "1") Long playId){
         return new ResponseEntity<>(animalStoreService.findPlayInfo(storeId, playId), HttpStatus.OK);
     }
 
 
     // 예약 정보 기입
+
     @GetMapping("/stores/books")
     public ResponseEntity postReservation(@RequestBody ReservationRequest reservationRequest){
 
@@ -155,8 +165,9 @@ public class StoreController {
     }
 
     // 동물 상세 정보 제공.
+    @ApiOperation(value = "animal_id 동물 상세 정보 가져오기")
     @GetMapping("/stores/{animal_id}/animal_detail")
-    public ResponseEntity<AnimalDetailResponse> getAnimalInfo(@PathVariable(name = "animal_id") Long animalId){
+    public ResponseEntity<AnimalDetailResponse> getAnimalInfo(@PathVariable(name = "animal_id") @ApiParam(name = "동물 id", required = true, example = "1") Long animalId){
         AnimalDetailResponse animalDetailResponse = new AnimalDetailResponse(
                 animalService.findAnimalInfo(animalId),
                 animalStoreService.findStoreSubDto(animalId),
@@ -168,8 +179,9 @@ public class StoreController {
     }
     
     // 동물 종 정보 제공
+    @ApiOperation(value = "species_id에 해당하는 종 정보 가져오기")
     @GetMapping("/stores/{species_id}/species_detail")
-    public ResponseEntity<SpeciesDetailResponse> getSpeciesInfo(@PathVariable(name = "species_id") Long speciesId){
+    public ResponseEntity<SpeciesDetailResponse> getSpeciesInfo(@PathVariable(name = "species_id")  @ApiParam(name = "종 id", required = true, example = "1")Long speciesId){
         SpeciesDetailResponse speciesDetailResponse = new SpeciesDetailResponse(
                 speciesService.findSpeciesInfo(speciesId),
                 feedService.findFeedListforSpecies(speciesId)
@@ -178,8 +190,9 @@ public class StoreController {
     }
 
     // 예약 정보 제공
+    @ApiOperation(value = "book_id에 해당하는 예약 내역 가져오기")
     @GetMapping("/stores/books/{book_id}")
-    public ResponseEntity<ReservationResponse> showReservationInfo(@PathVariable(name = "book_id") Long bookId){
+    public ResponseEntity<ReservationResponse> showReservationInfo(@PathVariable(name = "book_id") @ApiParam(name = "예약 id", required = true, example = "1") Long bookId){
         return new ResponseEntity<>(reservationService.getReservatonResult(bookId), HttpStatus.OK);
     }
 
