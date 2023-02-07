74d2739df968078dc471ba75e81d85dcf012e537
seunghee <kanada11416@gmail.com>
2023-02-07 16:08:16 +0900

    update : play entity 수정 play type 삭제

--- owner/src/main/java/com/marizoo/owner/entity/Play.java
+++ owner/src/main/java/com/marizoo/owner/entity/Play.java
@@ -19,9 +19,6 @@ public class Play extends BaseEntity {
 
     private LocalDateTime playDateTime;
 
-    @Enumerated(EnumType.STRING)
-    private PlayType playType;
-
     private String title;
 
     private String description;
