877047dc2a5356157086f55e3c60cf5a4a6971a6
한유경 <ygo65312@naver.com>
2023-02-08 11:18:09 +0900

    update : 환경변수명 변경

--- my-little-jurassic-buisness/src/react-app-env.d.ts
+++ my-little-jurassic-buisness/src/react-app-env.d.ts
@@ -3,6 +3,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: "development" | "production" | "test";
     readonly PUBLIC_URL: string;
-    readonly REACT_APP_OPENVIDU_URL: string;
+    readonly REACT_APP_API_URL: string;
   }
 }
