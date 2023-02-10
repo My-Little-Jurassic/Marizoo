d95bef9a8a79a3e1cd76f9bbeb82da21b057c2fc
한유경 <ygo65312@naver.com>
2023-02-10 16:49:18 +0900

    update : 방 생성 및 세션을 통한 client 간 통신 함수 작성

--- my-little-jurassic-buisness/src/pages/Broadcast.tsx
+++ my-little-jurassic-buisness/src/pages/Broadcast.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import styled from "styled-components";
 import { useState } from "react";
 import {
@@ -7,16 +7,22 @@ import {
   BroadcastVoteContainer,
 } from "../components/Broadcast";
 import { IBroadcastSetting } from "../types";
-import { IBroadcastStatus, IVote, TConnectionId, TUserId } from "../types/Broadcast";
+import {
+  IBroadcastStatus,
+  IBroadcastVariable,
+  IVote,
+  TConnectionId,
+  TUserId,
+} from "../types/Broadcast";
 import { postBroadcast } from "../api";
 import BroadcastVoteModal from "../components/Broadcast/BroadcastVoteModal";
-import { ConnectionEvent, OpenVidu } from "openvidu-browser";
-import { postOpenViduToken } from "../api/openvidu";
+import { OpenVidu } from "openvidu-browser";
 
 const Broadcast = () => {
   // 방송설정 STATE
   const [broadcastSetting, setBroadcastSetting] = useState<IBroadcastSetting>({
     id: "1",
+    status: "DEFAULT",
     title: "",
     description: "",
     thumbnail: null,
@@ -31,25 +37,46 @@ const Broadcast = () => {
     viewers: 0,
     likes: 0,
     vote: { options: [], voteStatus: "default", winnerFeed: 0 },
-    status: "DEFAULT",
   });
-  const { animalIdList } = broadcastSetting;
-  const { sessionId, viewers, likes, status, vote } = broadcastStatus;
+  const { animalIdList, status } = broadcastSetting;
+  const { viewers, likes, vote } = broadcastStatus;
 
   // OpenVidu를 위한 값
   const videoRef = useRef<HTMLVideoElement>(null);
-  const viewerRef = useRef<number>(0);
-  const likeRef = useRef<number>(0);
+  const variableRef = useRef<IBroadcastVariable>({
+    viewers: 0,
+    likes: 0,
+    vote: { options: [], voteStatus: "default", winnerFeed: 0 },
+  });
   const viewerMap = useRef<Map<TUserId, TConnectionId>>(new Map());
   const connectionMap = useRef<Map<TConnectionId, TUserId>>(new Map());
+  const roomInfoInterval = useRef<NodeJS.Timer>();
   const OV = useMemo(() => new OpenVidu(), []);
   const session = useMemo(() => OV.initSession(), [OV]);
+  useEffect(() => {
+    return () => {
+      if (broadcastSetting.status === "ONAIR") endBroadcast();
+    };
+  }, []);
+  useEffect(() => {
+    switch (broadcastSetting.status) {
+      case "ONAIR":
+        roomInfoInterval.current = setInterval(signalRoomInfo, 1000);
+        return;
+      case "FINISH":
+        clearInterval(roomInfoInterval.current);
+        return;
+      case "DEFAULT":
+      case "RESERVE":
+      default:
+        return;
+    }
+  }, [broadcastSetting]);
 
   // 세션 입장 함수
-  const joinSession = async () => {
-    const token = await postOpenViduToken(sessionId);
+  const joinSession = async (token: string, setting: IBroadcastSetting) => {
     await session.connect(token);
-    const { videoDevice } = broadcastSetting;
+    const { videoDevice } = setting;
     const newPublisher = await OV.initPublisherAsync(undefined, {
       audioSource: undefined, // The source of audio. If undefined default microphone
       videoSource: videoDevice, // The source of video. If undefined default webcam
@@ -60,20 +87,35 @@ const Broadcast = () => {
       insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
       mirror: false, // Whether to mirror your local video or not
     });
-
     session.publish(newPublisher);
-    newPublisher.addVideoElement(videoRef.current!);
+    if (videoRef.current) newPublisher.addVideoElement(videoRef.current);
   };
   // 세션 퇴장 함수
   const leaveSession = () => {
     session.disconnect();
+    setBroadcastSetting({ ...broadcastSetting, status: "FINISH" });
+    // TODO: 서버에 방송 결과 전송
+  };
+  // 방 정보 제공 함수
+  const signalRoomInfo = () => {
+    session.signal({
+      data: JSON.stringify({
+        numberOfViewers: variableRef.current.viewers,
+        numberOfLikes: variableRef.current.likes,
+      }),
+      to: [],
+      type: "roomInfo",
+    });
+    setBroadcastStatus({
+      ...broadcastStatus,
+      ...variableRef.current,
+    });
   };
 
   // btn events
   // 방송시작 함수
-  const startBroadcast = (setting: IBroadcastSetting) => {
-    setBroadcastSetting(setting);
-
+  const startBroadcast = async (setting: IBroadcastSetting) => {
+    console.log("startBroadcast");
     // 요청을 위한 데이터 폼을 생성
     // TODO: 유효한 데이터 삽입중인지 확인 필요
     const { id, title, description, animalIdList, thumbnail } = setting;
@@ -88,26 +130,38 @@ const Broadcast = () => {
     formData.append("broadcastInfo", new Blob([broadcastInfo], { type: "application/json" }));
     if (thumbnail) formData.append("img", thumbnail);
 
-    postBroadcast(formData).then((res) => {
+    const token = await postBroadcast(formData).then((res) => {
       // 요청에 대한 응답처리 부분
-      // TODO: 응답 확인 후 sessionId, pk 할당
-      setBroadcastStatus({ ...broadcastStatus, sessionId: "123", pk: 1, status: "ONAIR" });
+      const { broadcastId, sessionId, connectionToken } = res.data;
+      console.log("sessionId:", sessionId);
+      console.log("broadcastId:", broadcastId);
+      setBroadcastStatus({ ...broadcastStatus, sessionId, pk: broadcastId });
+      return connectionToken;
     });
+
+    await joinSession(token, setting);
+    setBroadcastSetting({ ...setting, status: "ONAIR" });
   };
   // 방송종료 함수
-  const endBroadcast = () => {
-    // setBroadcastStatus({ ...broadcastStatus, status: "FINISH" });
-    setBroadcastStatus({ ...broadcastStatus, status: "DEFAULT" });
+  const endBroadcast = async () => {
+    console.log("endBroadcast");
+    setBroadcastStatus({ ...broadcastStatus, ...variableRef.current });
+    // setBroadcastStatus({ ...broadcastStatus, status: "DEFAULT" });
 
-    session.signal({
+    // 방송 종료전 투표가 진행중이라면 투표를 종료한다.
+    if (broadcastStatus.vote.voteStatus === "proceeding") await finishVote();
+    await session.signal({
       data: undefined,
       to: [],
       type: "finish",
     });
+    leaveSession();
   };
   // 투표 시작 함수
   const startVote = (vote: IVote) => {
+    console.log("startVote");
     console.log(vote);
+    variableRef.current.vote = vote;
     setBroadcastStatus({ ...broadcastStatus, vote });
 
     const { options } = vote;
@@ -118,15 +172,17 @@ const Broadcast = () => {
     });
   };
   // 투표 종료 함수
-  const finishVote = () => {
-    const vote = broadcastStatus.vote;
+  const finishVote = async () => {
+    console.log("finishVote");
+    const vote = variableRef.current.vote;
     if (vote === null) return;
     vote.winnerFeed = vote.options.sort((a, b) => b.numberOfVotes - a.numberOfVotes)[0].id;
     vote.voteStatus = "finish";
+    variableRef.current.vote = vote;
     setBroadcastStatus({ ...broadcastStatus, vote });
 
     // 투표 결과 반환
-    session.signal({
+    await session.signal({
       data: String(vote.winnerFeed),
       to: [],
       type: "voteFinish",
@@ -136,6 +192,7 @@ const Broadcast = () => {
   // **openvidu signal events**
   //  새로운 세션 연결 시 welcome SIGNAL 호출
   session.on("connectionCreated", (e) => {
+    console.log("connection created");
     const { options, winnerFeed, voteStatus } = vote;
     session.signal({
       data: JSON.stringify({
@@ -149,19 +206,21 @@ const Broadcast = () => {
   });
   // 유저가 나가면 해당 유저 정보 삭제
   session.on("connectionDestroyed", (e) => {
-    const connectionId = e.connection!.connectionId;
+    console.log("connection destroyed");
+    const connectionId = e.connection.connectionId;
     const userId = connectionMap.current.get(connectionId);
     connectionMap.current.delete(connectionId);
-    viewerMap.current.delete(userId!);
+    if (userId) viewerMap.current.delete(userId);
   });
   // thankYou SIGNAL 감지시 해당 유저 정보 저장
   session.on("signal:thankYou", (e) => {
+    console.log("signal thankYou");
     if (e.data && e.from) {
       const connectionId = e.from.connectionId;
       const userId = e.data;
 
       if (!viewerMap.current.get(e.data)) {
-        viewerRef.current++;
+        variableRef.current.viewers++;
         connectionMap.current.set(connectionId, userId);
         viewerMap.current.set(userId, connectionId);
       }
@@ -169,12 +228,20 @@ const Broadcast = () => {
   });
   // like SIGNAL 감지시 좋아요 수 변경
   session.on("signal:like", (e) => {
-    if (e.data) likeRef.current++;
-    else likeRef.current--;
+    console.log("signal like");
+    if (e.data) variableRef.current.likes++;
+    else variableRef.current.likes--;
   });
   // vote SIGNAL 감지시 투표수 반영
   session.on("signal:vote", (e) => {
-    console.log("signal: vote |", e.data);
+    console.log("signal vote");
+    const vote = variableRef.current.vote;
+    const options = vote.options;
+    if (e.data) return;
+    const feedId = Number(e.data);
+    const feedIndex = options.findIndex((feed) => feed.id === feedId);
+    options[feedIndex].numberOfVotes++;
+    variableRef.current.vote = { ...vote, options };
   });
 
   return (
@@ -188,7 +255,6 @@ const Broadcast = () => {
           initSetting={broadcastSetting}
           startBroadcast={startBroadcast}
           endBroadcast={endBroadcast}
-          status={status}
         />
       </div>
       <BroadcastVoteModal startVote={startVote} animalIdList={animalIdList} />
