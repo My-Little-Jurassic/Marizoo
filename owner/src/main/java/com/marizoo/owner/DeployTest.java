e388ed018bea4d507a662fac2d308499b5c09677
heesootory <93choiheesoo@gmail.com>
2023-02-10 10:35:38 +0900

    update: owner_yml_ssl_delete

--- owner/src/main/java/com/marizoo/owner/DeployTest.java
+++ owner/src/main/java/com/marizoo/owner/DeployTest.java
@@ -1,5 +1,6 @@
 package com.marizoo.owner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeployTest {
 
     @GetMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return "welcome to the operator service!!!!";
     }
+
+
 }
