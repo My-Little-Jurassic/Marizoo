4d7ad30ef28eab06a21284b2da52a7fdbccdcb42
seunghee <kanada11416@gmail.com>
2023-02-12 00:41:34 +0900

    fix : 충돌 해결

--- user/src/main/java/com/marizoo/user/dto/broadcast_dto/RelatedBroadcastDto.java
+++ user/src/main/java/com/marizoo/user/dto/broadcast_dto/RelatedBroadcastDto.java
@@ -14,11 +14,7 @@ public class RelatedBroadcastDto {
     }
 
     @QueryProjection
-<<<<<<< HEAD:user/src/main/java/com/marizoo/user/dto/broadcast_dto/RelatedBroadcastDto.java
     public RelatedBroadcastDto(Long id, String sessionId, String title, String thumbnail){
-=======
-    public SearchBroadcastDto(Long id, String sessionId, String title, String thumbnail){
->>>>>>> 1b341438ac601d5aaac8c221f82ce78b77b78650:user/src/main/java/com/marizoo/user/dto/broadcast_dto/SearchBroadcastDto.java
         this.id = id;
         this.sessionId = sessionId;
         this.title = title;
