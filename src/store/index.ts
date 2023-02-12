f52f59ff6d6a6483951ee3dd1993b83ad1f47096
ysu6691 <ysu6691@naver.com>
2023-02-12 15:53:47 +0900

    fix : ovSlice 삭제, 방 퇴장 시 불필요한 const 삭제

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,10 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import userReducer from "./userSlice";
 import modalReducer from "./modalSlice";
 import broadcastReducer from "./broadcastSlice";
-import ovReducer from "./ovSlice";
 
 export const store = configureStore({
   // 로그인 유지용 access-token store
-  reducer: { user: userReducer, modal: modalReducer, broadcast: broadcastReducer, ov: ovReducer },
+  reducer: { user: userReducer, modal: modalReducer, broadcast: broadcastReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
