0a933a26c4d4ced8d6a7be1761e23a22818ce4a0
seungbok3240 <seungbok3240@gmail.com>
2023-02-13 13:07:50 +0900

    add : createdBy, lastModifiedBy

--- user/src/main/java/com/marizoo/user/UserApplication.java
+++ user/src/main/java/com/marizoo/user/UserApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,8 +24,19 @@ public class UserApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+				.map(SecurityContext::getAuthentication)
+				.map(authentication -> {
+					Collection<? extends GrantedAuthority> auth = authentication.getAuthorities();
+					boolean isUser = auth.contains(new SimpleGrantedAuthority("ROLE_USER"));
+					if (isUser) return authentication.getName();
+					return null;
+				});
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
-
 	}
 }
--- user/src/main/java/com/marizoo/user/entity/common/BaseEntity.java
+++ user/src/main/java/com/marizoo/user/entity/common/BaseEntity.java
@@ -2,26 +2,36 @@ package com.marizoo.user.entity.common;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Optional;
 
 @Getter
-@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedBy
