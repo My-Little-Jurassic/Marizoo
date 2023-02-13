225e884a8350443a17ec9aa0ff38680a0ad0d297
heesootory <93choiheesoo@gmail.com>
2023-02-14 00:47:21 +0900

    update: secret info -> hide

--- user/src/main/java/com/marizoo/user/DeployCheck.java
+++ user/src/main/java/com/marizoo/user/DeployCheck.java
@@ -5,14 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/api/user/")
 @Slf4j
 public class DeployCheck {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome() throws UnknownHostException {
         log.info("배포 health-check");
-        return "welcome to the user service!!!!!!@@!!!!zzzzfff slack!!!!! nginx!!!";
+        InetAddress ipAddress = InetAddress.getLocalHost();
+        return String.format("welcome to the user service!!   "
+            + "my ip is " + ipAddress);
     }
 }
