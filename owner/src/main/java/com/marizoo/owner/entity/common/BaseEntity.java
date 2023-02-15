d0365af66db711a755dadbf13a5ed03e5288d1c9
seunghee <kanada11416@gmail.com>
2023-02-16 00:17:02 +0900

    add : baseEntity 적용 등록일, 수정일 추적

--- owner/src/main/java/com/marizoo/owner/entity/common/BaseEntity.java
+++ owner/src/main/java/com/marizoo/owner/entity/common/BaseEntity.java
@@ -3,7 +3,10 @@ package com.marizoo.owner.entity.common;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -11,11 +14,25 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class BaseEntity {
 
+    @Column(updatable = false)
     private String createdBy;
 
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     private String lastModifiedBy;
 
     private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        lastModifiedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        lastModifiedDate = LocalDateTime.now();
+    }
 }
