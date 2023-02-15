3bc2c5e11cfbcc7b1e53d5913c93d52e0378dd8d
seunghee <kanada11416@gmail.com>
2023-02-15 23:16:51 +0900

    update : api/user/broadcasts 최근 15개의 onair 방송 가져오기로 변경

--- user/src/main/java/com/marizoo/user/repository/broadcast_repo/BroadcastRepository.java
+++ user/src/main/java/com/marizoo/user/repository/broadcast_repo/BroadcastRepository.java
@@ -3,6 +3,8 @@ package com.marizoo.user.repository.broadcast_repo;
 import com.marizoo.user.dto.broadcast_dto.BroadcastDto;
 import com.marizoo.user.entity.Broadcast;
 import com.marizoo.user.entity.BroadcastStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +12,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BroadcastRepository extends JpaRepository<Broadcast, Long>, BroadcastRepositoryCustom {
-    // 현재 방송 중인 목록 가져오기
-    List<Broadcast> findByStatus(BroadcastStatus status);
+    // 현재 방송 중인 목록 가져오기)
+    Page<Broadcast> findByStatus(BroadcastStatus status, Pageable pageable);
 
     @Query("select b from Broadcast b where b.animalStore.id = :storeId " +
             "and b.status = com.marizoo.user.entity.BroadcastStatus.ONAIR")
